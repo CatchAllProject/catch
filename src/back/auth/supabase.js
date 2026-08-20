14bac6504f345bb250f29536c49c6d6c458fd1ba
IsaacLanzoni <117325190+IsaacLanzoni@users.noreply.github.com>
2026-08-20 12:09:03 -0300

    .

--- src/back/auth/supabase.js
+++ src/back/auth/supabase.js
@@ -1,3 +1,6 @@
-
+const supabaseClient = window.supabase.createClient(
+    'https://onfkalldpevudcztvdwy.supabase.co',
+    'sb_publishable_m_xmv5n07mbMuDdWDB_eAg_A__1GIRj'
+);
 
 window.supabaseClient = supabaseClient;
